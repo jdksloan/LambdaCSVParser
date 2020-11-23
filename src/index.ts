763cc0a23a00079e4f2bc7fc5db5b17fc7ca2c8d
Jethro Sloan <jdksloan@gmail.com>
2020-11-23 21:28:55 +0100

    Simulated the unavailability of the end point

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,49 @@
+// import { ApiProcessor } from './lib/processing/ApiProcessor';
+// import { OrdersProcessor } from './lib/processing/OrdersProcessor';
+// import { Context, S3Event } from 'aws-lambda';
+// import { S3Controller } from './lib/storage/S3Controller';
+
+// const options = {
+//   host: 'qgc7c2xwhg.execute-api.eu-central-1.amazonaws.com',
+//   port: 443, // or 443 for https
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// };
+
+// exports.handler = async (event: S3Event, context: Context) => {
+//   const processor = new ApiProcessor('/Prod/order', options);
+//   const bkt = event.Records[0].s3.bucket.name;
+//   const key = event.Records[0].s3.object.key;
+//   const s3Controller = new S3Controller();
+//   const doc = await s3Controller.getObject({
+//     Bucket: bkt,
+//     Key: key,
+//   });
+//   if (doc.Body) {
+//     await new OrdersProcessor(processor).process(doc.Body.toString());
+//   }
+//   return;
+// };
+
+import * as path from 'path';
+import { Order } from './lib/parser/models/Order';
 import { ApiProcessor } from './lib/processing/ApiProcessor';
 import { OrdersProcessor } from './lib/processing/OrdersProcessor';
-import { Context, S3Event } from 'aws-lambda';
-import { S3Controller } from './lib/storage/S3Controller';
+import * as fs from 'fs';
+const csv = fs.readFileSync(path.resolve(__dirname, '../test/output-2.csv'));
 
 const options = {
-  host: 'qgc7c2xwhg.execute-api.eu-central-1.amazonaws.com',
-  port: 443, // or 443 for https
+  host: 'localhost',
+  port: 7779, // or 443 for https
   headers: {
     'Content-Type': 'application/json',
   },
 };
 
-exports.handler = async (event: S3Event, context: Context) => {
-  const processor = new ApiProcessor('/Prod/order', options);
-  const bkt = event.Records[0].s3.bucket.name;
-  const key = event.Records[0].s3.object.key;
-  const s3Controller = new S3Controller();
-  const doc = await s3Controller.getObject({
-    Bucket: bkt,
-    Key: key,
-  });
-  if (doc.Body) {
-    await new OrdersProcessor(processor).process(doc.Body.toString());
-  }
-  return;
-};
+const processor: ApiProcessor<Order> = new ApiProcessor('/api/v1/test/', options);
+
+console.time('Load test');
+new OrdersProcessor(processor).process(csv.toString()).then(() => {
+  console.timeEnd('Load test');
+});
--- src/lib/processing/OrdersProcessor.ts
+++ src/lib/processing/OrdersProcessor.ts
@@ -1,58 +1,68 @@
 import { FlowUtils } from './../utils/FlowUtils';
-import { Queue } from '../data/Queue';
 import { IProcess } from '../interfaces/IProcess';
 import { OrderParser } from '../parser/OrderParser';
 import { Order } from '../parser/models/Order';
 
 export class OrdersProcessor implements IProcess<string, Promise<void>> {
   private _processor: IProcess<Order, Promise<any>>;
+  private _retryAttempts: number;
+  private _orderReport: { fail: number; success: number };
 
-  constructor(processor: IProcess<Order, Promise<any>>) {
+  constructor(processor: IProcess<Order, Promise<any>>, retryAttempts: number = 15) {
     this._processor = processor;
+    this._retryAttempts = retryAttempts;
+    this._orderReport = { fail: 0, success: 0 };
   }
+
   /**
-   * Parses an array of orders
+   * Processes a raw string of orders
+   *
    * @param {string} raw
-   * @return {*}  {Order[]}
-   * @memberof OrdersParser
+   * @return {*}  {Promise<void>}
+   * @memberof OrdersProcessor
    */
   public async process(raw: string): Promise<void> {
     const rawOrders = raw.split('\n');
     const orderParser: OrderParser = new OrderParser();
     rawOrders.pop();
-    const orders: Queue<string> = new Queue(...rawOrders);
-    let processed = 0;
 
-    while (!orders.empty) {
-      const next = orders.dequeue();
-
-      if (!next) {
-        break;
-      }
-
+    for (const rawOrder of rawOrders) {
       try {
-        const order = orderParser.parse(next);
-        try {
-          const result = await this._processor.process(order);
-
-          if (result) {
-            if (result === 200) {
-              processed++;
-            } else if (result === 429) {
-              FlowUtils.sleep(500);
-              orders.enqueue(next);
-            } else {
-              console.error(`Failed to put the order ${next}`);
-            }
-          }
-        } catch (error) {
-          console.error(`Failed to put the order ${next} with error ${error}`);
-        }
+        const order = orderParser.parse(rawOrder);
+        await this._put(order);
       } catch (error) {
-        console.error(`Failed to parse the order ${next} with error ${error}`);
+        console.error(`Failed to parse the order ${rawOrder} with error ${error}`);
       }
     }
 
-    console.info(`${processed} orders processed`);
+    console.info(
+      `Order report: orders succeeded: ${this._orderReport.success}, orders failed: ${this._orderReport.fail}`,
+    );
+  }
+
+  private async _put(order: Order, retry?: number) {
+    if (!retry) {
+      retry = 0;
+    }
+
+    try {
+      const result = await this._processor.process(order);
+      if (result && result === 200) {
+        console.log(`Order: ${order.id} put!`);
+        this._orderReport.success++;
+        return;
+      } else if (retry === this._retryAttempts) {
+        console.error(`Failed to put the order ${order.id} with after ${this._retryAttempts} attempts!`);
+        this._orderReport.fail++;
+        return;
+      } else {
+        retry++;
+        FlowUtils.sleep(50);
+        await this._put(order, retry);
+      }
+    } catch (error) {
+      this._orderReport.fail++;
+      console.error(`Failed to put the order ${order.id} with error ${error}`);
+    }
   }
 }
